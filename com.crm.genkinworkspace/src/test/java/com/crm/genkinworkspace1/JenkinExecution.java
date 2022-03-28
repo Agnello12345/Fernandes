ff44a8ad4ff80e6ed0139962689eb071361ed612
PC <PC@DESKTOP-HH2KTU2>
2022-03-28 16:11:29 +0530

    commit 3

---
+++ com.crm.genkinworkspace/src/test/java/com/crm/genkinworkspace1/JenkinExecution.java
@@ -0,0 +1,18 @@
+package com.crm.genkinworkspace1;
+
+import org.testng.annotations.Test;
+
+public class JenkinExecution {
+
+
+	@Test
+		public void m1() {
+			System.out.println("jenkin running");
+
+	}
+	@Test
+		public void m2() {
+			System.out.println("jenkin executed");
+		}
+
+}
